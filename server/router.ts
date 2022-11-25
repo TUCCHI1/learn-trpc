69b2e1aadbd0889eaaf770cf0c14f399df7842d8
TUCCHI1 <tutikikin1@gmail.com>
2022-11-25 21:44:10 +0900

    add query procedure

--- server/router.ts
+++ server/router.ts
@@ -2,6 +2,38 @@ import { initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
 
-const appRouter = t.router({});
+interface User {
+  id: string;
+  name: string;
+}
+
+const userList: User[] = [
+  {
+    id: "1",
+    name: "TUCCHI1",
+  },
+];
+
+const appRouter = t.router({
+  // この時点では入力は不明
+  // クライアントは何でも送ってくる可能性がある
+  // なので、特定のデータ型を仮定することはしない
+  userById: t.procedure
+    .input((val: unknown) => {
+      // 値がstringであれば、その値を返す
+      // この値が文字列であることをTypeScriptは認識する
+      if (typeof val === "string") return val;
+
+      // 入力が文字列でなかった場合
+      // プロシージャを実行せず、エラーを投げる
+      throw new Error(`Invalid input: ${typeof val}`);
+    })
+    .query((req) => {
+      const { input } = req;
+
+      const user = userList.find((u) => u.id === input);
+      return user;
+    }),
+});
 
 export type AppRouter = typeof appRouter;
